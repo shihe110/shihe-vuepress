e7fadf5def713ef053965822ed77500271834ea1
shihe <github110@126.com>
2020-08-22 23:35:00 +0800

    修改路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,20 +23,16 @@ module.exports = {
 				{text: 'spring-cloud', link: '/springcloud/'},
 				{text: 'spring-security', link: '/springsecurity/'}
 			]},
-			{text: 'NoSql', items: [
-					{text: 'redis', link: '/redis/'}
-				]},
-			{text: 'MQ', items: [
-					{text: '消息中间件', link: '/mq/'}
-				]},
 			{text: 'Database', items: [
-				{text: 'db', link: '/db/'}
+				{text: 'db', link: '/db/'},
+				{text: 'redis', link: '/redis/'}
 			]},
 			{text: '前端', items: [
 				{text: 'vue', link: '/vue/'},
 				{text: 'js', link: '/js/'}
 			]},
 			{text: '互联网', items: [
+				{text: '消息中间件', link: '/mq/'},
 				{text: '互联网', link: '/internet/'},
 				{text: '面试', link: '/interview/'}
 			]},
